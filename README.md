851b86fa7f282c08104cf9603a1906badc8f8f7c
Tommy Gaessler <tommy.gaessler@gmail.com>
2017-08-24 15:30:01 -0600

    turned into static html site

---
+++ README.md
@@ -0,0 +1,5 @@
+# [Used Calculators](http://tommygaessler.com/usedcalculators)
+
+Built using HTML and CSS!
+
+A Christmas themed e-commerce website I made to sell calculators!
--- thank-you.php
+++
@@ -1,7 +0,0 @@
-<?php include('header.php');?>
-    <body>
-        <h1 class="center">Thanks for contacting us! We will get back with you as soon as possible!</h1>
-        <div class="buttonwrapper">
-            <a href="index.php"><button type="button" class="btn btn-primary button"><i class="fa fa-arrow-left"></i>Take me back to Used Calculators!<i class="fa fa-calculator"></i></button></a>
-        </div>
-<?php include('footer.php');?>
